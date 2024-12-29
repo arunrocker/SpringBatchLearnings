ebcf0572d3f0ef0b6d5acc0855601dfa5f674ea1
arunrocker <m.arunkumarmar12@gmail.com>
2024-12-29 21:19:53 +0530

    Async Spring Batch Processing

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableBatchProcessing
+@EnableAsync
+@ComponentScan("com.springbatch.createJob,com.tasklet.customTasklet,com.example.listener,com.example.itemReader,com.example.itemProcessor,com.example.itemWriter,com.example.controller,com.example.service")
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/request/Request.java
@@ -0,0 +1,21 @@
+package com.example.request;
+
+public class Request {
+	
+	private String paramKey;
+	private String paramValue;
+	public String getParamKey() {
+		return paramKey;
+	}
+	public void setParamKey(String paramKey) {
+		this.paramKey = paramKey;
+	}
+	public String getParamValue() {
+		return paramValue;
+	}
+	public void setParamValue(String paramValue) {
+		this.paramValue = paramValue;
+	}
+	
+
+}
