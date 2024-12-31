2ce467172f881faa6bb6918852c1a17dff6130d2
arunrocker <m.arunkumarmar12@gmail.com>
2024-12-31 07:53:02 +0530

    Make sure to add inputFile in program Arguments

---
+++ src/main/java/com/example/itemProcessor/FirstItemProcessor.java
@@ -0,0 +1,16 @@
+package com.example.itemProcessor;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstItemProcessor implements ItemProcessor<Integer, Long>{
+
+	@Override
+	public Long process(Integer item) throws Exception {
+		System.out.println("Insiden ItemProcessor");
+		return Long.valueOf(item+20);
+	}
+	
+
+}
