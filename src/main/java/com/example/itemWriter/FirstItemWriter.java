b984b53b24d3e928424bac920a5bce6af0009ea7
arunrocker <m.arunkumarmar12@gmail.com>
2025-01-02 07:52:10 +0530

    Xml ItemReader Learnings

---
+++ src/main/java/com/example/itemWriter/FirstItemWriter.java
@@ -0,0 +1,22 @@
+package com.example.itemWriter;
+
+import java.util.List;
+
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+import com.example.model.StudentCsv;
+import com.example.model.StudentJson;
+import com.example.model.StudentXml;
+
+@Component
+public class FirstItemWriter implements ItemWriter<StudentXml>{
+
+	@Override
+	public void write(List<? extends StudentXml> items) throws Exception {
+		System.out.println("Inside ItemWriter");
+		items.stream().forEach(System.out::println);
+		
+	}
+
+}
